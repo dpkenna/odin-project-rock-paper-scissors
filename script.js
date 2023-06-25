e69a8d193eb5dbe3c8c5b30d97345739035eb26e
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-06-24 21:05:23 -0700

    Add function to play the game

--- script.js
+++ script.js
@@ -10,15 +10,41 @@ function getComputerChoice(array) {
     // Return the item at the random index
     return array[randomChoice];
 }
-// Call the function to make a random choice and assign it to a variable
+
+// Play a game
+function playRound(playerSelection, computerSelection) {
+    // Compare computer choice to player choice and announce the result
+    switch() {
+        case playerSelection == "rock" && computerSelection == "paper":
+            return("You lose! Paper beats rock.");
+            break;
+        case playerSelection == "rock" && computerSelection == "scissors":
+            return("You win! Rock beats scissors.");
+            break;
+        case playerSelection == "paper" && computerSelection == "rock":
+            return("You win! Paper beats rock.");
+            break;
+        case playerSelection == "paper" && computerSelection == "scissors":
+            return("You lose! Scissors beats paper.");
+            break;
+        case playerSelection == "scissors" && computerSelection == "paper":
+            return("You win! Scissors beats paper.");
+            break;
+        case playerSelection == "scissors" && computerSelection == "rock":
+            return("You lose! Rock beats scissors.");
+            break;
+        default:
+            return("It's a tie.")
+    }
+}
+
+// Call the getComputerChoice function to make a random choice and assign it to a variable
 const cpuChoice = getComputerChoice(choices);
 
 // Have player choose rock, paper, or scissors and change it to lowercase
 const playerChoice = prompt("Choose rock, paper, or scissors");
 const plyChoice = playerChoice.toLowerCase();
 
-// Compare computer choice to player choice
-// Announce the winner
 // Play 5 games
     // Keep score for 5 games
     // Announce the winner at the end of 5 games
