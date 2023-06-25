a879e14a9ae38cf26c438683a98fea9617ca4640
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-06-24 17:02:37 -0700

    Set up initial pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+// Have computer choose rock, paper, or scissors at random
+// Have player choose rock, paper, or scissors
+// Make player choice case-insensitive
+// Compare computer choice to player choice
+// Announce the winner
+// Play 5 games
+    // Keep score for 5 games
+    // Announce the winner at the end of 5 games
