c0660842be0a4601d3a6959b5567a037049ece72
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-06-24 21:18:11 -0700

    Change switch to if else if because switch was always returning default

--- script.js
+++ script.js
@@ -14,29 +14,22 @@ function getComputerChoice(array) {
 // Play a game
 function playRound(playerSelection, computerSelection) {
     // Compare computer choice to player choice and announce the result
-    switch() {
-        case playerSelection == "rock" && computerSelection == "paper":
+        if (playerSelection == "rock" && computerSelection == "paper") {
             return("You lose! Paper beats rock.");
-            break;
-        case playerSelection == "rock" && computerSelection == "scissors":
+        } else if (playerSelection == "rock" && computerSelection == "scissors") {
             return("You win! Rock beats scissors.");
-            break;
-        case playerSelection == "paper" && computerSelection == "rock":
+        } else if (playerSelection == "paper" && computerSelection == "rock") {
             return("You win! Paper beats rock.");
-            break;
-        case playerSelection == "paper" && computerSelection == "scissors":
+        } else if (playerSelection == "paper" && computerSelection == "scissors") {
             return("You lose! Scissors beats paper.");
-            break;
-        case playerSelection == "scissors" && computerSelection == "paper":
+        } else if (playerSelection == "scissors" && computerSelection == "paper") {
             return("You win! Scissors beats paper.");
-            break;
-        case playerSelection == "scissors" && computerSelection == "rock":
+        } else if (playerSelection == "scissors" && computerSelection == "rock") {
             return("You lose! Rock beats scissors.");
-            break;
-        default:
-            return("It's a tie.")
+        } else {
+            return("It's a tie.");
+        }
     }
-}
 
 // Call the getComputerChoice function to make a random choice and assign it to a variable
 const cpuChoice = getComputerChoice(choices);
@@ -44,6 +37,7 @@ const cpuChoice = getComputerChoice(choices);
 // Have player choose rock, paper, or scissors and change it to lowercase
 const playerChoice = prompt("Choose rock, paper, or scissors");
 const plyChoice = playerChoice.toLowerCase();
+console.log(playRound(plyChoice, cpuChoice));
 
 // Play 5 games
     // Keep score for 5 games
