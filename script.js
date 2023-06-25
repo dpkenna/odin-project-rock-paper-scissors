f3d6c0bf7ec613b2e65d77cf2312678b1be45686
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-06-24 17:22:12 -0700

    Add function to have computer make a random choice

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// Set up the choices of rock, paper, or scissors
+const choices = ["rock", "paper", "scissors"];
+
 // Have computer choose rock, paper, or scissors at random
+// Function to choose 1 item from an array
+function getComputerChoice(array) {
+    // Get a random index for the array
+    const randomChoice = Math.floor(Math.random() * choices.length);
+
+    // Return the item at the random index
+    return array[randomChoice];
+}
+// Call the function to make a random choice
+getComputerChoice(choices);
+
 // Have player choose rock, paper, or scissors
 // Make player choice case-insensitive
 // Compare computer choice to player choice
