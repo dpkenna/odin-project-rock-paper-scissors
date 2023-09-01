a0da43e5b1fec73724010e18e194d9326945570f
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-31 19:41:23 -0700

    Play a game when the button is clicked

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         let playerChoice = button.id;
+        game();
     });
 });
 
